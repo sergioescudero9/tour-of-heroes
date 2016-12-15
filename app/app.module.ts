92aaf917e067cbe069c4af6fba983a60897076b1
Sergio Andres Escudero Ramirez <saescudero@cognox.com>
2016-12-15 10:38:44 -0500

    Se finaliza el enrutador

--- app/app.module.ts
+++ app/app.module.ts
@@ -8,12 +8,17 @@ import { AppRoutingModule } from './app-routing.module';
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
+import './rxjs-extensions';
+
 
 import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard.component';
 import { HeroesComponent }      from './heroes.component';
 import { HeroDetailComponent }  from './hero-detail.component';
 import { HeroService }          from './hero.service';
+import { HeroSearchComponent }  from './hero-search.component';
+
+
 
 
 @NgModule({
@@ -24,12 +29,14 @@ import { HeroService }          from './hero.service';
     InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
-  declarations: [
+    declarations: [
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroSearchComponent
   ],
+
   providers: [ HeroService ],
   bootstrap: [ AppComponent ]
 })
